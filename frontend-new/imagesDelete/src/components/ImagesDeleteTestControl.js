bfb634918134ee885199a64471655691dcd5b861
Kutliev Amir Rovshenovich <akutliev@ozon.ru>
2024-09-15 12:45:39 +0700

    intermediate result

---
+++ frontend-new/imagesDelete/src/components/ImagesDeleteTestControl.js
@@ -0,0 +1,3 @@
+export default function ImagesDeleteTestControl() {
+    return <div>Это тестовый компонент из проекта Удаления лайков</div>
+}
---
+++ frontend-new/imagesUpload/src/components/ImagesUploadTestControl.js
@@ -0,0 +1,3 @@
+export default function ImagesUploadTestControl() {
+    return <div>Это тестовый компонент из проекта Profile</div>
+}
